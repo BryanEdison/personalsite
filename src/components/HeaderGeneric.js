542ee2587d424318d98768dfad9835858a6de685
Edison Alulema <ealulem1@binghamton.edu>
2018-09-09 21:47:50 -0400

    changed cover

--- src/components/HeaderGeneric.js
+++ src/components/HeaderGeneric.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeaderGeneric = (props) => (
     <header id="header">
-        <h1>Edison Bryan</h1>
+        <h1>Bryan Edison</h1>
         <p></p>
     </header>
 )
