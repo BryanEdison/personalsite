90fb6c42ad3f54af0cf970b4857ee05117a2dd3a
Edison Alulema <ealulem1@binghamton.edu>
2018-09-09 21:23:18 -0400

    changed cover

--- src/pages/bluecore.js
+++ src/pages/bluecore.js
@@ -12,9 +12,13 @@ class BlueCore extends React.Component {
       <div>
         <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
       <Header/>
+
+      <header className="major">
+                  <h2>Hey BlueCore</h2>
+                </header>
         <div id="main">
 
-            <h1>Hey BlueCore</h1>
+
             <p>I'm sincerely interested in working for you. I missed to formally meet you Fullstack's launch day but I hope that's not a deterrent in applying. It was still great meeting you John and Andrew!</p>
             <p>I graduated from Fullstack 3 months ago am currently a Software Engineer at SploreGuide, a tourism marketplace startup. I'm currently working on building out their React/React-Native front-end and integrating it with their MongoDB/GraphQL backend. </p>
         </div>
