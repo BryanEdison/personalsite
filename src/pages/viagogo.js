d16a7739dd2ad4a278395290965af0714cc91842
Edison Alulema <ealulem1@binghamton.edu>
2018-09-09 22:40:59 -0400

    changed cover

--- src/pages/viagogo.js
+++ src/pages/viagogo.js
@@ -9,7 +9,7 @@ import Link from 'gatsby-link'
 
 
 
-class BlueCore extends React.Component {
+class Viagogo extends React.Component {
 
   constructor(props) {
     super(props);
@@ -43,9 +43,9 @@ class BlueCore extends React.Component {
       <div className="spotlight">
         <div className="content">
       <header className="major">
-                  <h2>Hi BlueCore!</h2>
+                  <h2>Hi Viagogo!</h2>
             </header>
-            <p>I'm sincerely interested in working for you. I missed the chance to formally meet you at Fullstack's launch day but I hope that's not a deterrent in applying. It was great meeting you John and Andrew!</p>
+            <p>I'm sincerely interested in working for you. I missed the chance to formally meet you at Fullstack's launch day but I hope that's not a deterrent in applying. It was great meeting you Shiyang and Keya!</p>
             <p>I graduated from Fullstack 3 months ago and I am currently a Software Engineer at SploreGuide, a tourism marketplace startup. I'm currently working on building out their Web and Mobile apps using React, React-Native, MongoDB, Apollo-Express, and GraphQL. </p>
             <p>Prior to programming, I graduated from Binghamton University with a double degree in Economics and Sociology, and a minor in Chinese Studies. After attempting to innovate ride-sharing among college students with a web app in college, I became hooked into world of software development and couldn't be happier.</p>
         </div>
@@ -146,10 +146,10 @@ class BlueCore extends React.Component {
   }
 }
 
-export default BlueCore
+export default Viagogo
 
 export const pageQuery = graphql`
-  query BlueCoreQuery {
+  query ViagogoQuery {
     site {
       siteMetadata {
         title
